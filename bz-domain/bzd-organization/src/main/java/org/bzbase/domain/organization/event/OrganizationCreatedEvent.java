7ee16e4fbec0ad09c260c6112377356e03f4f4af
legendjw <legendjw@yeah.net>
2025-10-29 09:53:47 +0800

    feat(organization): 组织创建事件新增租户ID字段

--- bz-domain/bzd-organization/src/main/java/org/bzbase/domain/organization/event/OrganizationCreatedEvent.java
+++ bz-domain/bzd-organization/src/main/java/org/bzbase/domain/organization/event/OrganizationCreatedEvent.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.bzbase.domain.organization.Organization;
 import org.bzbase.library.ddd.event.AbstractDomainEvent;
 import org.bzbase.primitive.organization.OrganizationId;
+import org.bzbase.primitive.tenant.TenantId;
 
 import java.time.Instant;
 
@@ -12,10 +13,12 @@ import java.time.Instant;
  */
 @Getter
 public class OrganizationCreatedEvent extends AbstractDomainEvent {
+    private final TenantId tenantId;
     private final OrganizationId organizationId;
     private final String organizationName;
 
     public OrganizationCreatedEvent(Organization organization) {
+        this.tenantId = organization.getTenantId();
         this.organizationId = organization.getId();
         this.organizationName = organization.getName();
         this.occurredAt = Instant.now();
